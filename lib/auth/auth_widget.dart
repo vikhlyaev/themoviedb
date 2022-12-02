bf7f0923987272a1aa979ac01e2d768528849933
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-12-02 15:13:06 +0500

    Adds Auth screen

---
+++ lib/auth/auth_widget.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:themoviedb/theme/user_colors.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({super.key});
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: UserColors.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: UserColors.backgroundColor,
+        elevation: 0,
+        title: Text('Login to your account'),
+      ),
+      body: ListView(
+        children: [
+          _HeaderWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 25),
+          _FormWidget(),
+          SizedBox(height: 25),
+          Text(
+            'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.',
+          ),
+          SizedBox(height: 10),
+          OutlinedButton(
+            onPressed: () {},
+            child: Text('Register'),
+          ),
+          SizedBox(height: 25),
+          Text(
+            'If you signed up but didn`t get your verification email, click here to have it resent.',
+          ),
+          SizedBox(height: 10),
+          OutlinedButton(
+            onPressed: () {},
+            child: Text('Verify email'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget({super.key});
+
+  @override
+  State<_FormWidget> createState() => __FormWidgetState();
+}
+
+class __FormWidgetState extends State<_FormWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Username'),
+        SizedBox(height: 5),
+        TextField(),
+        SizedBox(height: 20),
+        Text('Password'),
+        SizedBox(height: 5),
+        TextField(
+          obscureText: true,
+        ),
+        SizedBox(height: 20),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                child: Text('Reset password'),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
