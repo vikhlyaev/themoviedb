325190efe61a043dec1fa5e69628c5b8a300a99f
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-12-04 14:48:57 +0500

    Adds Main Screen

---
+++ lib/main/main_screen_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MainScreenWidget extends StatefulWidget {
+  const MainScreenWidget({super.key});
+
+  @override
+  State<MainScreenWidget> createState() => _MainScreenWidgetState();
+}
+
+class _MainScreenWidgetState extends State<MainScreenWidget> {
+  int _selectedIndex = 0;
+
+  static const _widgetOptions = <Widget>[
+    Text('News'),
+    Text('Movies'),
+    Text('TV Shows')
+  ];
+
+  onSelectedBar(int index) {
+    if (_selectedIndex == index) return;
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Movie DB'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'News',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Movie',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            label: 'TV Shows',
+          ),
+        ],
+        onTap: onSelectedBar,
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+    );
+  }
+}
