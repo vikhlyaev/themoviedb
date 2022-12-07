3aef0ebf79f191f7b0c72becd01edd992f3cf10f
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-12-07 15:32:36 +0500

    Adds new route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:themoviedb/auth/auth_widget.dart';
+import 'package:themoviedb/details/details_widget.dart';
 import 'package:themoviedb/main/main_screen_widget.dart';
 import 'package:themoviedb/theme/app_theme.dart';
 
@@ -19,6 +20,14 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => AuthWidget(),
         '/main': (context) => MainScreenWidget(),
+        '/main/details': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments;
+          if (arguments is int) {
+            return DetailsWidget(id: arguments);
+          } else {
+            return DetailsWidget(id: 0);
+          }
+        }
       },
       initialRoute: '/',
       onGenerateRoute: (RouteSettings settings) {
