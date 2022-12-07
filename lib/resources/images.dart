bdba122e9d4dab11e75c4a7d5f144923f566f03d
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-12-07 15:33:18 +0500

    Adds new images

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -3,5 +3,7 @@ part of 'resources.dart';
 class Images {
   Images._();
 
+  static const String actor = 'assets/images/actor.jpg';
+  static const String backdrop = 'assets/images/backdrop.jpg';
   static const String poster = 'assets/images/poster.png';
 }
